5e6d05a7e7513cd19c1d18fdb91177008ef4c6cb
Jacques Fidelia Jr <jacques.fidelia@gmail.com>
2020-01-31 15:51:17 +0000

    third commit

--- exercises/10-Find_avg/app.py
+++ exercises/10-Find_avg/app.py
@@ -2,3 +2,10 @@ my_list = [2323,4344,2325,324413,21234,24531,2123,42234,544,456,345,42,5445,23,5
 
 #Your code here:
 
+def find_the_average(a):
+    avg = 0
+    for x in a:
+        avg = sum(a) / len(a)
+        return avg
+
+print(find_the_average(my_list))
