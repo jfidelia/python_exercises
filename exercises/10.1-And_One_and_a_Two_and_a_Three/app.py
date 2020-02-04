c050791fc022a97899d488bc2c1d6d2031cb2df6
Jacques Fidelia Jr <jacques.fidelia@gmail.com>
2020-02-04 14:26:42 +0000

    fourth commit

--- exercises/10.1-And_One_and_a_Two_and_a_Three/app.py
+++ exercises/10.1-And_One_and_a_Two_and_a_Three/app.py
@@ -4,4 +4,5 @@ contact = {
     "email": "test@test.com"
 }
 #Your code here:
-
+for x,y in contact.items():
+    print(x,y)
--- exercises/11-Nested_list/app.py
+++ exercises/11-Nested_list/app.py
@@ -2,5 +2,7 @@
 coordinatesList = [[33.747252,-112.633853],[-33.867886, -63.987],[41.303921, -81.901693],[-33.350534, -71.653268]]
 
 # Your code go here:
-
+longitude = coordinatesList([0][1])
+for x in coordinatesList:
+    print(x)
 
