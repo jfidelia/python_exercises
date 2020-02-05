bf551c02e4b50bb2b9bfea8024f36e17333d823a
Jacques Fidelia Jr <jacques.fidelia@gmail.com>
2020-02-05 14:04:47 +0000

    sixth commit

--- exercises/11-Nested_list/app.py
+++ exercises/11-Nested_list/app.py
@@ -2,12 +2,8 @@
 coordinates_list = [[33.747252,-112.633853],[-33.867886, -63.987],[41.303921, -81.901693],[-33.350534, -71.653268]]
 
 # Your code go here:
-longitude = []
-for x in coordinates_list:
-    for longitude in x:
-        longitude = coordinates_list[0][1]
-    
-    
 
-    print(longitude)
+for x in coordinates_list:
+  
+       print(x[1])
 
