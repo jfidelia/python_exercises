fac1ac390ad77661e2686c4e5694bb51cf648010
Jacques Fidelia Jr <jacques.fidelia@gmail.com>
2020-02-07 21:40:30 +0000

    Seventh Commit

--- exercises/12.5-Yes_and_no/app.py
+++ exercises/12.5-Yes_and_no/app.py
@@ -1,5 +1,11 @@
 theBools = [0,1,0,0,1,1,1,0,0,1,0,1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,1]
 
 #Your code go here:
-
+x = map(theBools, (0,1,0,1,1,1,0,0,1,0,1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,1))
+for x in range(0,len(theBools)):
+    if x == 1:
+        print("wiki")
+    else:
+        print("woko")
+print(list())
 
