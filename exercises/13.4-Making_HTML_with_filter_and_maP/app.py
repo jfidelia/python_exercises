6b122a83e03beeb281a7c53e5185076d834d75f7
Jacques Fidelia Jr <jacques.fidelia@gmail.com>
2020-02-08 22:34:46 +0000

    tenth commit

--- exercises/13.4-Making_HTML_with_filter_and_maP/app.py
+++ exercises/13.4-Making_HTML_with_filter_and_maP/app.py
@@ -9,4 +9,22 @@ all_colors = [
 ]
 
 #Your code go here:
+def filter_colors(items):
+    return items["sexy"] == True
+new_list = list(filter(filter_colors, all_colors))
 
+
+def generate_li(item):
+    return "<li>" + item["label"] + "</li>"
+
+
+array_of_lis = list(map(generate_li,new_list))
+
+string_of_lis = ''.join([str(elem) for elem in array_of_lis]) 
+
+print("<ul>"+string_of_lis+"</ul>")
+
+
+    #
+# html = list(map(lambda color: "<ul><li>" + color["label"] + "</li><li>", all_colors))
+# print(html)
