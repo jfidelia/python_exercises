0e8881993c2ec7deac3c7a9a3594e6b63471ff3f
Jacques Fidelia Jr <jacques.fidelia@gmail.com>
2020-02-17 23:01:43 +0000

    exercise 3

--- exercises/03-flip_list/app.py
+++ exercises/03-flip_list/app.py
@@ -1,5 +1,7 @@
 arr = [45, 67, 87, 23, 5,  32, 60]
 new_list = []
 #your code below:
-for x in range(0, len(arr), 1):
-    print(arr)
+for x in range(len(arr) - 1, -1, -1):
+    new_list.append(arr[::-1])
+
+print(new_list[x])
